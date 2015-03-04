51b2fe733010dc0104a96be08520e46db4e005f9
Patrick San Juan <pat.sanjuan@gmail.com>
2015-03-04 12:20:48 -0800

    added focus styling to searchbar on main page nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+	// Toggle focus style on searchbar in main page
+	$("#nav-searchbar").on("focus", "input", function() {
+		$(this).parent().addClass('search-focus');
+	});
+	$("#nav-searchbar").on("focusout", "input", function() {
+		$(this).parent().removeClass('search-focus');
+	});
+});
