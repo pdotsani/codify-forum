7802142038c2372181b37a69cfb8f25c4aa0f4f1
Patrick San Juan <pat.sanjuan@gmail.com>
2015-03-05 12:28:30 -0800

    added toggle replies & new reply feature, modified nav styles

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
 	$("#nav-searchbar").on("focusout", "input", function() {
 		$(this).parent().removeClass('search-focus');
 	});
+
+	// Show & hide replies/new reply
+	$("#show-reply").on("click", function() {
+		console.log('toggled show reply');
+		$(".replies").toggleClass('hidden').
+		$(".replies").toggleClass('wrapped-up');
+	});
+	$("#create-reply").on("click", function() {
+		console.log('toggled create reply');
+		$(".post-reply-section").toggleClass('hidden').
+		$(".post-reply-section").toggleClass('wrapped-up');
+	});
 });
